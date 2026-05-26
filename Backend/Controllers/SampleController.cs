d1bd7b2610314431761b1e70dfbdf35f316c906a
hazavi <sanchaiavila@gmail.com>
2026-05-26 08:55:09 +0200

    Add user secrets and Controller with sample file

---
+++ Backend/Controllers/SampleController.cs
@@ -0,0 +1,43 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Backend.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class SampleController(AppDbContext db) : ControllerBase
+{
+    // GET api/sample
+    [HttpGet]
+    public IActionResult GetAll()
+    {
+        return Ok();
+    }
+
+    // GET api/sample/{id}
+    [HttpGet("{id}")]
+    public IActionResult GetById(int id)
+    {
+        return Ok();
+    }
+
+    // POST api/sample
+    [HttpPost]
+    public IActionResult Create([FromBody] object body)
+    {
+        return CreatedAtAction(nameof(GetById), new { id = 0 }, body);
+    }
+
+    // PUT api/sample/{id}
+    [HttpPut("{id}")]
+    public IActionResult Update(int id, [FromBody] object body)
+    {
+        return NoContent();
+    }
+
+    // DELETE api/sample/{id}
+    [HttpDelete("{id}")]
+    public IActionResult Delete(int id)
+    {
+        return NoContent();
+    }
+}
