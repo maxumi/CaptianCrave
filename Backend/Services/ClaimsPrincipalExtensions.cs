1fc0d32c0a85b644d42b74e0b21b45db3b0c67f6
Max Felding <maxjac64@gmail.com>
2026-06-11 08:22:39 +0200

    added some comments and updated images

--- Backend/Services/ClaimsPrincipalExtensions.cs
+++ Backend/Services/ClaimsPrincipalExtensions.cs
@@ -2,8 +2,14 @@ using System.Security.Claims;
 
 namespace Backend.Services;
 
+
 public static class ClaimsPrincipalExtensions
 {
+    /// <summary>
+    /// Returns a user id from claim assocatied with user
+    /// </summary>
+    /// <param name="user"></param>
+    /// <returns></returns>
     public static int GetId(this ClaimsPrincipal user)
     {
         var id = user.FindFirstValue(ClaimTypes.NameIdentifier);
